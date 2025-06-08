d40e3a0184b0c55ebea554b1bcbd46691d47eff6
XAV59213 <153436936+XAV59213@users.noreply.github.com>
2025-06-08 09:45:29 +0200

    Update const.py

--- custom_components/jourferier/const.py
+++ custom_components/jourferier/const.py
@@ -20,3 +20,17 @@ PUBLIC_HOLIDAYS_2025 = {
     "11:11": "Armistice 1918",
     "25:12": "Noël"
 }
+
+HOLIDAY_DESCRIPTIONS = {
+    "Jour de l’An": "Bonne année ! Que cette nouvelle année soit pleine de joie et de réussite !",
+    "Lundi de Pâques": "Joyeuses Pâques ! Profitez de cette journée festive en famille !",
+    "Fête du Travail": "Bonne fête du travail ! Une journée pour célébrer les travailleurs !",
+    "Victoire 1945": "Hommage à la victoire de 1945 ! Une journée de mémoire et de paix.",
+    "Ascension": "Joyeuse Ascension ! Une journée spirituelle et de repos.",
+    "Lundi de Pentecôte": "Bonne fête de Pentecôte ! Une journée pour célébrer l'Esprit Saint.",
+    "Fête Nationale": "Vive la France ! Joyeux 14 juillet, fête nationale !",
+    "Assomption": "Bonne fête de l’Assomption ! Une journée de célébration mariale.",
+    "Toussaint": "Bonne Toussaint ! Un moment pour honorer les saints et les défunts.",
+    "Armistice 1918": "Hommage aux héros de 1918 ! Une journée de recueillement.",
+    "Noël": "Joyeux Noël ! Une fête chaleureuse pour partager amour et cadeaux !"
+}
